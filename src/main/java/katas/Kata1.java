82481d8c1f052467da8b01c10808a18157a72598
Jehison Galarza <jehisonsc045@gmail.com>
2022-05-25 18:41:50 -0500

    FuntionalProgramming Katas --- Kata 1 and Kata 2 resolved

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -5,8 +5,11 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -17,6 +20,20 @@ public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        List<Map> newMovies = movies.stream().map(movie -> {
+            HashMap<String, String> result = new HashMap<String, String>();
+            result.put("id", movie.getId().toString());
+            result.put("title", movie.getTitle());
+            return result;
+        }).collect(Collectors.toList());
+
+        return newMovies;
+    }
+
+    public static void main(String[] args) {
+        Kata1 exercise1 = new Kata1();
+        System.out.println(exercise1.execute());
     }
 }
+
+
--- src/main/java/katas/Kata2.java
+++ src/main/java/katas/Kata2.java
@@ -5,8 +5,11 @@ import com.google.common.collect.ImmutableMap;
 import model.Movie;
 import util.DataUtil;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Chain filter() and map() to collect the ids of videos that have a rating of 5.0
@@ -17,6 +20,16 @@ public class Kata2 {
     public static List<Integer> execute() {
         List<Movie> movies = DataUtil.getMovies();
 
-        return ImmutableList.of(1, 2, 3);
+         return movies.stream().filter(movie -> movie.getRating() == 5.0).map(item -> {
+            Integer result = item.getId();
+            return result;
+        }).collect(Collectors.toList());
+
+
+    }
+
+    public static void main(String[] args) {
+        Kata2 exercise1 = new Kata2();
+        System.out.println(exercise1.execute());
     }
 }
