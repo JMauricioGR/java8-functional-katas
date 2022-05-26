7b2e502aa892896fac0e09dfaf5421a618e4b595
Jehison Galarza <jehisonsc045@gmail.com>
2022-05-25 23:43:59 -0500

    FuntionalProgramming Katas --- Kata 7 solved

--- src/main/java/katas/Kata7.java
+++ src/main/java/katas/Kata7.java
@@ -3,12 +3,15 @@ package katas;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import model.Bookmark;
+import model.BoxArt;
 import model.Movie;
 import model.MovieList;
 import util.DataUtil;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve the id, title, and smallest box art url for every video
@@ -19,6 +22,25 @@ public class Kata7 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+        return movieLists.stream()
+                .flatMap(item -> item.getVideos().stream())
+                .map(item -> {
+                    HashMap<String, String> result = new HashMap<>();
+                    result.put("id", item.getId().toString());
+                    result.put("title", item.getTitle());
+                    result.put("boxart", item
+                            .getBoxarts()
+                            .stream()
+                            .reduce((acum, elemn) ->
+                                acum.getWidth() > elemn.getWidth()?elemn:acum
+                            ).map(url -> url.getUrl()).orElseThrow());
+                    return result;
+                }).collect(Collectors.toList());
+        //return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys", "boxart", "url"));
+    }
+
+    public static void main(String[] args) {
+        Kata7 exercise = new Kata7();
+        System.out.println(exercise.execute());
     }
 }
